2b9e7f36211f86a2a312ef0dbbdca1a2a518ff98
julio1304 <79971753+julio1304@users.noreply.github.com>
2024-10-22 09:34:14 +0000

    Add files via upload

---
+++ PostmanCloneUI/Dashboard.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace PostmanCloneUI
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            formHeader = new Label();
+            apiLabel = new Label();
+            apiText = new TextBox();
+            resultsText = new TextBox();
+            callApi = new Button();
+            statusStrip = new StatusStrip();
+            systemStatus = new ToolStripStatusLabel();
+            resultsLabel = new Label();
+            statusStrip.SuspendLayout();
+            SuspendLayout();
+            // 
+            // formHeader
+            // 
+            formHeader.AutoSize = true;
+            formHeader.Font = new Font("Segoe UI", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            formHeader.Location = new Point(773, 22);
+            formHeader.Name = "formHeader";
+            formHeader.Size = new Size(317, 60);
+            formHeader.TabIndex = 0;
+            formHeader.Text = "Postman Clone";
+            // 
+            // apiLabel
+            // 
+            apiLabel.AutoSize = true;
+            apiLabel.Location = new Point(495, 130);
+            apiLabel.Name = "apiLabel";
+            apiLabel.Size = new Size(69, 41);
+            apiLabel.TabIndex = 1;
+            apiLabel.Text = "API:";
+            // 
+            // apiText
+            // 
+            apiText.BorderStyle = BorderStyle.FixedSingle;
+            apiText.Location = new Point(594, 127);
+            apiText.Name = "apiText";
+            apiText.Size = new Size(746, 47);
+            apiText.TabIndex = 2;
+            // 
+            // resultsText
+            // 
+            resultsText.BackColor = Color.White;
+            resultsText.BorderStyle = BorderStyle.FixedSingle;
+            resultsText.Location = new Point(495, 267);
+            resultsText.Multiline = true;
+            resultsText.Name = "resultsText";
+            resultsText.ReadOnly = true;
+            resultsText.ScrollBars = ScrollBars.Both;
+            resultsText.Size = new Size(1058, 398);
+            resultsText.TabIndex = 3;
+            // 
+            // callApi
+            // 
+            callApi.Location = new Point(1376, 127);
+            callApi.Name = "callApi";
+            callApi.Size = new Size(178, 47);
+            callApi.TabIndex = 4;
+            callApi.Text = "Go";
+            callApi.UseVisualStyleBackColor = true;
+            callApi.Click += callApi_Click;
+            // 
+            // statusStrip
+            // 
+            statusStrip.BackColor = Color.LightGray;
+            statusStrip.ImageScalingSize = new Size(20, 20);
+            statusStrip.Items.AddRange(new ToolStripItem[] { systemStatus });
+            statusStrip.Location = new Point(0, 885);
+            statusStrip.Name = "statusStrip";
+            statusStrip.Size = new Size(1700, 37);
+            statusStrip.TabIndex = 5;
+            statusStrip.Text = "statusStrip1";
+            // 
+            // systemStatus
+            // 
+            systemStatus.BackColor = Color.White;
+            systemStatus.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            systemStatus.Name = "systemStatus";
+            systemStatus.Size = new Size(76, 31);
+            systemStatus.Text = "Ready";
+            // 
+            // resultsLabel
+            // 
+            resultsLabel.AutoSize = true;
+            resultsLabel.Location = new Point(495, 223);
+            resultsLabel.Name = "resultsLabel";
+            resultsLabel.Size = new Size(111, 41);
+            resultsLabel.TabIndex = 6;
+            resultsLabel.Text = "Results";
+            // 
+            // Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1700, 922);
+            Controls.Add(resultsLabel);
+            Controls.Add(statusStrip);
+            Controls.Add(callApi);
+            Controls.Add(resultsText);
+            Controls.Add(apiText);
+            Controls.Add(apiLabel);
+            Controls.Add(formHeader);
+            Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(6);
+            Name = "Dashboard";
+            Text = "Postman";
+            statusStrip.ResumeLayout(false);
+            statusStrip.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label formHeader;
+        private Label apiLabel;
+        private TextBox apiText;
+        private TextBox resultsText;
+        private Button callApi;
+        private StatusStrip statusStrip;
+        private Label resultsLabel;
+        private ToolStripStatusLabel systemStatus;
+    }
+}
---
+++ PostmanCloneUI/Dashboard.cs
@@ -0,0 +1,37 @@
+using PostmanCloneLibrary;
+
+namespace PostmanCloneUI;
+
+public partial class Dashboard : Form
+{
+    private readonly IApiAccess api = new ApiAccess();
+
+    public Dashboard()
+    {
+        InitializeComponent();
+    }
+
+    private async void callApi_Click(object sender, EventArgs e)
+    {
+        systemStatus.Text = "Calling API...";
+        resultsText.Text = "";
+
+        if (api.IsValidUrl(apiText.Text) ==  false)
+        {
+            systemStatus.Text = "Invalid URL";
+            return;
+        }
+
+        try
+        {
+            resultsText.Text = await api.CallApiAsync(apiText.Text);
+
+            systemStatus.Text = "Ready";
+        }
+        catch (Exception ex)
+        {
+            resultsText.Text = "Error: " + ex.Message;
+            systemStatus.Text = "Error";
+        }
+    }
+}
