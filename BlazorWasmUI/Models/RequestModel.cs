bf55f8407358dcbe2f849444fd5782b690022076
julio1304 <79971753+julio1304@users.noreply.github.com>
2024-10-26 10:37:49 +0000

    Add files via upload

---
+++ BlazorWasmUI/Models/RequestModel.cs
@@ -0,0 +1,17 @@
+﻿using PostmanCloneLibrary;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWasmUI.Models;
+
+public class RequestModel
+{
+	[Required]
+    [Display(Name = "Http Verb Type")]
+	public HttpAction? HttpVerbSelection { get; set; }
+
+    [Required]
+    [Url]
+    [Display(Name = "API Url")]
+    public string? ApiText { get; set; }
+    public string Body { get; set; } = "";
+}
