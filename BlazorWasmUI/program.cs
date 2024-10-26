bb5134959321d098c75edff45847552c5d29b906
julio1304 <79971753+julio1304@users.noreply.github.com>
2024-10-26 10:36:52 +0000

    Update program.cs

--- BlazorWasmUI/program.cs
+++ BlazorWasmUI/program.cs
@@ -1,1 +1,14 @@
+using BlazorWasmUI;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using PostmanCloneLibrary;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IApiAccess, ApiAccess>();
+
+await builder.Build().RunAsync();
 
